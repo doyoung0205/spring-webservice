d4bf74bd6a78b1781f120ca15654ecbc92c80347
DESKTOP-NNMFK6A\DoYoung <doyoung0205@naver.com>
2018-11-14 16:11:27 +0900

    ASCSACS

--- spring-webservice/src/test/java/com/jojoidu/webservice/SpringWebserviceApplicationTests.java
+++ spring-webservice/src/test/java/com/jojoidu/webservice/SpringWebserviceApplicationTests.java
@@ -2,15 +2,18 @@ package com.jojoidu.webservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest
 public class SpringWebserviceApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		
+		
 	}
 
 }
