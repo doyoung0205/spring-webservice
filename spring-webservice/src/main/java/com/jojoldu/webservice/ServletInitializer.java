0261f62696f1a939bf8b6ff386b6eec94f51e180
DESKTOP-NNMFK6A\DoYoung <doyoung0205@naver.com>
2018-11-14 13:58:09 +0900

    CCCCCC

--- spring-webservice/src/main/java/com/jojoldu/webservice/ServletInitializer.java
+++ spring-webservice/src/main/java/com/jojoldu/webservice/ServletInitializer.java
@@ -1,8 +1,7 @@
 package com.jojoldu.webservice;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
