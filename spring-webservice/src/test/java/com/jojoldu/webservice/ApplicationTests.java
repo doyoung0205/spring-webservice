a990bf0793a27fc3fc17ca47dcb8337cbd131c9b
DESKTOP-NNMFK6A\DoYoung <doyoung0205@naver.com>
2018-11-16 09:16:09 +0900

    CSCAQE!

--- spring-webservice/src/test/java/com/jojoldu/webservice/ApplicationTests.java
+++ spring-webservice/src/test/java/com/jojoldu/webservice/ApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
-public class SpringWebserviceApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
